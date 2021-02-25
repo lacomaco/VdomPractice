5e79d63cd059d83df2eae7e16ec7f4752d5eec3b
lacomaco <lacomaco122@gmail.com>
2021-02-26 00:14:07 +0900

    feature : JSX가 함수형 컴포넌트도 렌더링 하도록 기능 추가
    el이 함수라면 함수형 컴포넌트임으로, 해당 컴포넌트를 호출하는 기능을
    추가하였습니다.

--- src/lib/core/MacoVdom.js
+++ src/lib/core/MacoVdom.js
@@ -1,6 +1,10 @@
 import { h } from "snabbdom/h";
+import { isFunction } from "../../util/is";
 const MacoVDom = {
   createElement: (el, props, ...children) => {
+    if (isFunction(el)) {
+      return el();
+    }
     return h(
       el,
       props,
