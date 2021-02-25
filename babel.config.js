02bb966f81eb31f03ae4e31967159128a1e338df
lacomaco <lacomaco122@gmail.com>
2021-02-25 23:05:25 +0900

    JSX 트랜스파일링 함수 이름 변경
    기본적으로 React의 JSX 트랜스 파일링 플러그인을 쓰되
    React.createElement가 아닌 내가 만들 maco.createElement을 쓰도록 수정

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
     [
       "@babel/preset-react",
       {
-        pragma: "babo.createElement",
+        pragma: "maco.createElement",
         throwIfNamespace: false,
       },
     ],
