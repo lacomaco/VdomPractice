7e284717f59140e53b49dd71d2447e0249fe01a5
lacomaco <lacomaco122@gmail.com>
2021-02-25 23:43:08 +0900

    feature : 배열 형태의 JSX도 기능하도록 수정
    React의 JSX map 처럼 배열 형태의 JSX도 렌더링 되도록 수정

--- src/lib/core/MacoVdom.js
+++ src/lib/core/MacoVdom.js
@@ -1,7 +1,11 @@
 import { h } from "snabbdom/h";
 const MacoVDom = {
   createElement: (el, props, ...children) => {
-    return h(el, props, children);
+    return h(
+      el,
+      props,
+      children.flatMap((x) => x)
+    );
   },
 };
 
