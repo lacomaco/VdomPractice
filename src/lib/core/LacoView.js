74b39e59164a932231da042e14e85a627bcdccfc
lacomaco <lacomaco122@gmail.com>
2021-03-15 23:49:36 +0900

    라이프사이클을 props에 주입해주는 함수 생성

--- src/lib/core/LacoView.js
+++ src/lib/core/LacoView.js
@@ -1,5 +1,7 @@
 import { patch } from "./Maco";
+import { h } from "snabbdom/h";
 import MacoCore from "./Maco";
+import { isString } from "../../util/is";
 const { jsxToJson } = MacoCore;
 export default class LacoView {
   constructor(componentFN) {
@@ -8,14 +10,29 @@ export default class LacoView {
   }
 
   render(props, effects) {
-    this.__vNode = this.componentFN(props, effects);
+    this.vDomInfo = this.componentFN(props, effects);
     return this.__vNode;
   }
+
+  bindLifeCycle() {}
 
   update() {}
 
   reconcile(prev, next) {
     this.__vNode = patch(prev, next);
     return this.__vNode;
+  }
+
+  createElement({ el, props, children }) {
+    return h(
+      el,
+      props,
+      children.map((child) => {
+        if (isString(child)) {
+          return child;
+        }
+        this.createElement(child);
+      })
+    );
   }
 }
