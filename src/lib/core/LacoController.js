7e661c79d7dacc5e7073e8d2db7748a5811ecb44
lacomaco <lacomaco122@gmail.com>
2021-03-16 22:34:13 +0900

    useState,useEffect 기능 이전

--- src/lib/core/LacoController.js
+++ src/lib/core/LacoController.js
@@ -17,7 +17,52 @@ export default class LacoController {
     );
     return this.view.__vNode;
   }
-  __update(props) {}
-  useState(initVal) {}
-  useEffect(callback, diffArray = []) {}
+  __update(props) {
+    this.model.cursor = 0;
+    this.model.afterHookCursor = 0;
+  }
+  useState(initVal) {
+    const cursor = this.model.cursor;
+    const hooks = this.model.hooks;
+
+    hooks[cursor] = hooks[cursor] || [
+      initVal,
+      (val) => {
+        if (hooks[cursor] !== val) {
+          hooks[val][0] = val;
+          this.__update(this.model.props);
+        }
+      },
+    ];
+
+    this.model.cursor++;
+    return [...hooks[cursor]];
+  }
+  useEffect(callback, diffArray = []) {
+    const cursor = this.model.cursor;
+    const hooks = this.model.hooks;
+    const afterCursor = this.model.afterCursor;
+    const afterHooks = this.model.afterHooks;
+
+    let cleanUp = undefined;
+
+    if (!hooks[cursor]) {
+      cleanUp = callback();
+      hooks[cursor] = callback;
+    } else {
+      const isChanged = !hooks[cursor].every((elm, i) => elm === diffArray[i]);
+
+      if (isChanged) {
+        cleanUp = callback();
+        hooks[cursor] = diffArray;
+      }
+    }
+
+    if (cleanUp) {
+      this.model.afterCursor++;
+      afterHooks[afterCursor] = cleanUp;
+    }
+
+    this.model.cursor++;
+  }
 }
--- src/lib/core/LacoModel.js
+++ src/lib/core/LacoModel.js
@@ -2,6 +2,7 @@ export default class LacoModel {
   constructor() {
     this.props = null;
     this.cursor = 0;
+    this.afterHookCursor = 0;
     this.hooks = [];
     this.afterHooks = [];
     this.hook = {};
@@ -22,4 +23,8 @@ export default class LacoModel {
   resetCursor() {
     this.cursor = 0;
   }
+
+  setUnMountHook(cn) {}
+
+  getHooks() {}
 }
