8272acf05a873c2f2d071859166261dd35689381
lacomaco <lacomaco122@gmail.com>
2021-03-17 00:45:28 +0900

    생성시 아예 새로운 LacoController 인스턴스 객체를 만들어 주는 Factory 함수 생성

--- src/lib/core/LacoFactory.js
+++ src/lib/core/LacoFactory.js
@@ -3,8 +3,10 @@ import LacoModel from "./LacoModel";
 import LacoView from "./LacoView";
 
 export default function (componentFN) {
-  const view = new LacoView(componentFN);
-  const model = new LacoModel();
-  const controller = new LacoController(view, model);
-  return controller;
+  return () => {
+    const view = new LacoView(componentFN);
+    const model = new LacoModel();
+    const controller = new LacoController(view, model);
+    return controller;
+  };
 }
