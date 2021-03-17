b4eda06be1896cffd021fde4b5b680707df008ff
lacomaco <lacomaco122@gmail.com>
2021-03-17 20:19:29 +0900

    컴포넌트 식별자 생성

--- src/lib/core/LacoFactory.js
+++ src/lib/core/LacoFactory.js
@@ -3,10 +3,11 @@ import LacoModel from "./LacoModel";
 import LacoView from "./LacoView";
 
 export default function (componentFN) {
+  const componentMarker = Symbol();
   return () => {
     const view = new LacoView(componentFN);
     const model = new LacoModel();
-    const controller = new LacoController(view, model);
+    const controller = new LacoController(view, model, componentMarker);
     return controller;
   };
 }
