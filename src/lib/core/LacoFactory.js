cb8be76c321a6726848fa8cbbe5d5c49eb4d29aa
lacomaco <lacomaco122@gmail.com>
2021-03-22 15:32:22 +0900

    [#10] view 객체에 model 의존성 추가
    - View객체에서 Model을 자주 참조하는 경우가 많아 의존성으로
      추가하였습니다.

--- src/lib/core/LacoFactory.js
+++ src/lib/core/LacoFactory.js
@@ -5,8 +5,8 @@ import LacoView from "./LacoView";
 export default function (componentFN) {
   const componentMarker = Symbol();
   return () => {
-    const view = new LacoView(componentFN);
     const model = new LacoModel();
+    const view = new LacoView(componentFN, model);
     const controller = new LacoController(view, model, componentMarker);
     return controller;
   };
